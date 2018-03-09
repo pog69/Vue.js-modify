17f56d437d2f5e72b8b8f893d97ad4ae90a69a1a
高庄優人 <takashoyuto@koushouyuuninnoMacBook-puro.local>
2018-03-09 16:00:25 +0900

    modify

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <input v-model="message">
+  </div>
+</template>
+
+<script>
+  new Vue {
+    el: '#app',
+    data () {
+      return {
+        message: 'Hello!'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
